17106d0f92cf17d30eb8031991ac0312940d9fb9
Nisharu <nisharu99@gmail.com>
2023-01-01 20:36:56 +0530

    Signed-off-by: Nisharu <nisharu99@gmail.com>

--- resources/views/add.blade.php
+++ resources/views/add.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Add Company Form - Laravel 9 CRUD</title>
+    <title>Add Category Form - Laravel 9 CRUD</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 
